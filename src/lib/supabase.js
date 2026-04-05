07c8925d4b2bc5fb52b045c4c14a373512977894
Niclas Harrer <niclasharrer@MacBook-Air-von-Niclas.local>
2026-04-05 21:15:28 +0200

    Fix anon key

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -2,5 +2,5 @@ import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
   'https://vjimgvsueahdtjtgiiqd.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZqaW1ndnN1ZWFoZHRqdGdpaXFkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM4MDg5NDgsImV4cCI6MjA1OTM4NDk0OH0.KQ-0H3Wr3onXHsDLhbZIuv_98JgAavdy0XuNi507Q64'
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZqaW1ndnN1ZWFoZHRqdGdpaXFkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU0MDg5NDgsImV4cCI6MjA5MDk4NDk0OH0.KQ-0H3Wr3onXHsDLhbZIuv_98JgAavdy0XuNi507Q64'
 )
